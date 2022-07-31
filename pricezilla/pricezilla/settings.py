1a559bce5f231e62c6bf0c8657bfa3cfe0f0f0ac
nurudeen2432 <nurudeendurowade@gmail.com>
2022-07-31 01:05:06 +0100

    commit
    batch commit

--- pricezilla/pricezilla/settings.py
+++ pricezilla/pricezilla/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -130,7 +135,4 @@ AUTH_USER_MODEL = 'user.User'
 
 MEDIA_URL = 'images/'
 
-STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
-)
 
---
+++ pricezilla/static/scripts/index.js
@@ -0,0 +1,8 @@
+// var classList = document.getElementById('navscroll').classList;
+
+// var maxWidth576 = window.matchMedia("(max-width: 576px)");
+// var windowSize = window.innerWidth;
+
+// function tolu()
+// { (windowSize >=576) ? classList.remove('container') : classList.add('container')}
+
