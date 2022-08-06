738b344f9f57ee17ab406d9045ba459209636899
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-08-06 21:19:59 +0000

    added allauth configuration in settings.py

--- pricezilla/pricezilla/urls.py
+++ pricezilla/pricezilla/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('accounts/', include('user.urls')),
-    path("accounts/",include("django.contrib.auth.urls"))
+    path("accounts/",include("django.contrib.auth.urls")),
+    path('accounts/', include('allauth.urls')),
 ]
